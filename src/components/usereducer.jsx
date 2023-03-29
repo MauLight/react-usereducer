deaef1cd7f993a683df7635c943ba0d349a470f7
MauLight <maulisseluz@gmail.com>
2023-03-29 01:44:24 -0300

    update usereducer exercise

--- src/components/usereducer.jsx
+++ src/components/usereducer.jsx
@@ -2,17 +2,17 @@ import { useReducer } from "react"
 
 
 function reducer(state, action) {
-    switch(action.type) {
+    switch (action.type) {
         case 'incremental_age': {
             return {
-                name: state.name,
+                ...state,
                 age: state.age + 1
             }
         }
         case 'changed_name': {
             return {
+                ...state,
                 name: action.nextName,
-                age: state.age
             };
         }
     }
@@ -20,18 +20,23 @@ function reducer(state, action) {
 }
 
 export const Reducer = () => {
-    const [state, dispatch] = useReducer(reducer, { age: 38 }); //dispatch changes state. State has initial value given.
+    const [state, dispatch] = useReducer(reducer, { name: '', age: 38 }); //dispatch changes state. State has initial value given.
 
     console.log(state);
 
     const handleClick = () => {
-        dispatch({type: 'incremental_age'});
+        dispatch({ type: 'incremental_age' });
+    };
+
+    const handleChange = (e) => {
+        dispatch({type: 'changed_name', nextName: e.target.value});
     }
 
     return (
         <div className="my-3">
             <button onClick={handleClick} className="btn rounded-0 border my-2">Increment Age</button>
-            <h5>Hello there, you're {state.age} years old!</h5>
+            <h5>Hello there {state.name}, you're {state.age} years old!</h5>
+            <input type="text" className="form-control" value={state.name} onChange={handleChange} />
         </div>
     )
 
